e90e7190f1ba6bc7a1b530f993d878815ef5a759
miowoo <miowoo821@gmail.com>
2018-01-22 11:38:35 +0800

    各種select語法

--- app/src/main/java/com/example/student/a2018012201_00/MainActivity.java
+++ app/src/main/java/com/example/student/a2018012201_00/MainActivity.java
@@ -56,4 +56,38 @@ public class MainActivity extends AppCompatActivity {
         c.moveToNext();//moveToFirst,游標移動到下一筆資料
         Log.d("DB", c.getString(1) + "," + c.getInt(2));
     }
+    public void click3(View v)//篩選方法1
+    {
+        File dbFile = new File(getFilesDir(), "student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READWRITE);
+        String strSql = "Select * from students where _id=?";
+        Cursor c = db.rawQuery(strSql, new String[] {"2"});
+        //Cursorj物件可以搜尋低一個引數裡的SQL語法裡面的問號，並用第二個引數的陣列依序填入SQL與法理的問號，有幾個物號陣列就有幾個值
+        c.moveToFirst();
+        Log.d("DB", c.getString(1) + "," + c.getInt(2));
+    }
+    public void click4(View v)//讀取方法2
+    {
+        File dbFile = new File(getFilesDir(), "student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READWRITE);
+        Cursor c = db.query("students", new String[] {"_id", "name", "score"}, null, null, null, null, null);
+       //db.query的
+        //第一個引數：選定這個資料庫物件的特定資料表
+        //第二個引數：這個資料表的欄位
+        //第三個引數：欲篩選欄位
+        //第四個引數：欲篩選的關鍵字
+        //第五個引數：
+        //第六個引數：
+        //第七個引數：排序方法
+        c.moveToFirst();
+        Log.d("DB", c.getString(1) + "," + c.getInt(2));
+    }
+    public void click5(View v)//篩選方法2
+    {
+        File dbFile = new File(getFilesDir(), "student.db");
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READWRITE);
+        Cursor c = db.query("students", new String[] {"_id", "name", "score"}, "_id=?", new String[] {"2"}, null, null, null);
+        c.moveToFirst();
+        Log.d("DB", c.getString(1) + "," + c.getInt(2));
+    }
 }
